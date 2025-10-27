0e680eb5ec6073f11d8a4e8370b0ac929a0d0aaf
stefanungureanu04 <stefan.ungureanu2004@gmail.com>
2025-10-27 21:30:05 +0200

    .

---
+++ CLIENT/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello from Client!\n");
+    return 0;
+}
---
+++ SERVER/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello from Server!\n");
+    return 0;
+}
